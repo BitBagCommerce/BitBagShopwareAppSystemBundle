13b685f3ae1362669f62f0d23174ae6848afd953
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-04-14 10:58:52 +0200

    ecs

--- tests/Response/FeedbackResponseTest.php
+++ tests/Response/FeedbackResponseTest.php
@@ -6,7 +6,6 @@ namespace BitBag\ShopwareAppSystemBundle\Tests\Response;
 
 use BitBag\ShopwareAppSystemBundle\Model\Feedback\Notification\Error;
 use BitBag\ShopwareAppSystemBundle\Response\FeedbackResponse;
-use function PHPUnit\Framework\assertEquals;
 use PHPUnit\Framework\TestCase;
 
 final class FeedbackResponseTest extends TestCase
